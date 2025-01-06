8409157774b3912ab3ce5dabda8e595ce86c31da
Daniel <vitordaniel2003g@gmail.com>
2025-01-06 02:41:40 -0300

    criando meu primeiro server, usando get ,retonrnado um json

--- src/controllers/podcast-controller.ts
+++ src/controllers/podcast-controller.ts
@@ -1,37 +1,23 @@
-# NOME DA APLICATIVO
-PodAPi
+import {IncomingMessage,request,ServerResponse} from "http";
 
-## DESCRIÇAO
-um app que eu posso listar os eps de diferentes podcastes 
-
-## DOMINIO
-podcast feito em video
-
-## features
-    -Lista os epsodios em sessoes de categoria
-        -[ciências,programação,humor,cultura nerd]
-    -filtrar episodios por nome podcast
-
-## como vou implementar
-    -Listar os epsodios podcasts em sessoes de categoria 
-        --retornar em uma api rest(json) nome do podcasrt,nome do epsodio, imagem de capa,link, category
-```js
-    [
+export const getListEps = async (request:IncomingMessage, response:ServerResponse) =>{
+    response.writeHead(200,{'content-Type': 'application/json'});
+    response.end(JSON.stringify([
         {
             podcastName: 'Inteligência Ltda',
             epsodio: 'TOKUSATSU: HERÓIS JAPONESES DO JASPION, CHANGEMAN E JIRAYA - Inteligência Ltda. Podcast #1067',
             idVideo: 'Ct3oyXuM9TE',
+            type: 'video',
             imagemCover: 'https://i.ytimg.com/vi/Ct3oyXuM9TE/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLA3C2b6ktV5FoDgZFi27KQX5_gXiw',
-            link: 'https://www.youtube.com/watch?v=Ct3oyXuM9TE',
-            category:[ciências,programação,humor,cultura nerd]   
+            category:['ciências','programação','humor','cultura nerd'],   
         },
         {
             podcastName: 'Inteligência Ltda',
             epsodio: 'DA INVENÇÃO DO COMPUTADOR À INTELIGÊNCIA ARTIFICIAL - FÁBIO AKITA - Inteligência Ltda. Podcast #1228',
             idVideo: 'jVxzpiDJ9Qk',
+            type: 'live',
             imagemCover: 'https://i.ytimg.com/vi/jVxzpiDJ9Qk/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLBbscebUoeAggbCiB1K8dkJquQFYA',
-            link: 'https://www.youtube.com/live/jVxzpiDJ9Qk',
-            category:[ciências,programação,humor,cultura nerd]   
+            category:['ciências','programação','humor','cultura nerd'],   
         }
-    ]
-```
+    ]))
+}
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,18 @@
-console.log('ola');
+import * as http from 'http';
+import {getListEps} from './controllers/podcast-controller';
+
+const server = http.createServer(
+    async (request: http.IncomingMessage, response: http.ServerResponse) => {
+
+        if(request.method === 'GET'){
+            await getListEps(request,response);
+        }
+
+    }
+);
+
+const port = process.env.PORT
+
+server.listen( port, () => {
+    console.log('Servidor iniciado na porta ' + port);
+});
